40adc6afd487933f1eecae407413f4002d300701
AcidBythorne <124373615+AcidBythorne@users.noreply.github.com>
2023-03-03 11:56:57 -0300

    Add files via upload

---
+++ Minhaaplicacaoa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.minhaaplicacaoa;
+
+/**
+ *
+ * @author Fortec
+ */
+public class Minhaaplicacaoa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
